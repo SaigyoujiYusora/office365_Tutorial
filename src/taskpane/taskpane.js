20075aeb598cfc7921ff0e2c826cfd056c99e84b
SaigyoujiYusora <misaka09983@gmail.com>
2023-04-12 19:26:41 +0800

    Word V5(Finish)

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -22,6 +22,8 @@ Office.onReady((info) => {
     document.getElementById("insert-image").onclick = () => tryCatch(insertImage);
     document.getElementById("insert-html").onclick = () => tryCatch(insertHTML);
     document.getElementById("insert-table").onclick = () => tryCatch(insertTable);
+    document.getElementById("create-content-control").onclick = () => tryCatch(createContentControl);
+    document.getElementById("replace-content-in-control").onclick = () => tryCatch(replaceContentInControl);
   }
 });
 
@@ -145,6 +147,28 @@ async function insertTable() {
       ["Sue", "719", "Havana"],
     ];
     secondParagraph.insertTable(3, 3, Word.InsertLocation.after, tableData);
+
+    await context.sync();
+  });
+}
+async function createContentControl() {
+  await Word.run(async (context) => {
+
+    const serviceNameRange = context.document.getSelection();
+    const serviceNameContentControl = serviceNameRange.insertContentControl();
+    serviceNameContentControl.title = "Service Name";
+    serviceNameContentControl.tag = "serviceName";
+    serviceNameContentControl.appearance = "Tags";
+    serviceNameContentControl.color = "blue";
+
+    await context.sync();
+  });
+}
+async function replaceContentInControl() {
+  await Word.run(async (context) => {
+
+    const serviceNameContentControl = context.document.contentControls.getByTag("serviceName").getFirst();
+    serviceNameContentControl.insertText("Fabrikam Online Productivity Suite", Word.InsertLocation.replace);
 
     await context.sync();
   });
