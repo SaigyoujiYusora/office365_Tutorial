7cb813a43b6d69deaa5c2cfb003aa298e4e6a839
SaigyoujiYusora <misaka09983@gmail.com>
2023-04-12 18:42:49 +0800

    Word V1 Push

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office, Word */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Word) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    // Assign event handlers and other initialization logic.
+    document.getElementById("insert-paragraph").onclick = () => tryCatch(insertParagraph);
+  }
+});
+
+async function insertParagraph() {
+  await Word.run(async (context) => {
+
+    const docBody = context.document.body;
+    docBody.insertParagraph("Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.",
+        Word.InsertLocation.start);
+
+    await context.sync();
+  });
+}
+
+/** Default helper for invoking an action and handling errors. */
+async function tryCatch(callback) {
+  try {
+    await callback();
+  } catch (error) {
+    // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+    console.error(error);
+  }
+}
