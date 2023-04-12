44365c6b230ee79d0bffea5273710c49db94b8e4
SaigyoujiYusora <misaka09983@gmail.com>
2023-04-12 19:20:36 +0800

    Word V3

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -14,6 +14,9 @@ Office.onReady((info) => {
     document.getElementById("apply-style").onclick = () => tryCatch(applyStyle);
     document.getElementById("apply-custom-style").onclick = () => tryCatch(applyCustomStyle);
     document.getElementById("change-font").onclick = () => tryCatch(changeFont);
+    document.getElementById("insert-text-into-range").onclick = () => tryCatch(insertTextIntoRange);
+    document.getElementById("insert-text-outside-range").onclick = () => tryCatch(insertTextBeforeRange);
+    document.getElementById("replace-text").onclick = () => tryCatch(replaceText);
   }
 });
 
@@ -64,6 +67,47 @@ async function changeFont() {
       bold: true,
       size: 18
     });
+
+    await context.sync();
+  });
+}
+async function insertTextIntoRange() {
+  await Word.run(async (context) => {
+
+    const doc = context.document;
+    const originalRange = doc.getSelection();
+    originalRange.insertText(" (M365)", Word.InsertLocation.end);
+
+    originalRange.load("text");
+    await context.sync();
+
+    doc.body.insertParagraph("Original range: " + originalRange.text, Word.InsertLocation.end);
+
+    await context.sync();
+  });
+}
+async function insertTextBeforeRange() {
+  await Word.run(async (context) => {
+
+    const doc = context.document;
+    const originalRange = doc.getSelection();
+    originalRange.insertText("Office 2019, ", Word.InsertLocation.before);
+
+    originalRange.load("text");
+    await context.sync();
+
+    doc.body.insertParagraph("Current text of original range: " + originalRange.text, Word.InsertLocation.end);
+
+    await context.sync();
+
+  });
+}
+async function replaceText() {
+  await Word.run(async (context) => {
+
+    const doc = context.document;
+    const originalRange = doc.getSelection();
+    originalRange.insertText("many", Word.InsertLocation.replace);
 
     await context.sync();
   });
